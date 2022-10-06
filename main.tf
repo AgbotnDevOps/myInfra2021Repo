879f42e2e6d2af5bbfe2942b8ed597db46e92020
Agbotn18 <111661840+Agbotn18@users.noreply.github.com>
2022-10-06 17:05:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,3 +60,14 @@ tags= {
     Name = "my_elastic_ip"
   }
 }
+
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
